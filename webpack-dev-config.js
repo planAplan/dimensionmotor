c89a5cb3e62fc39ce6879b9f3ec192a482e74335
unknown <forzhangy@163.com>
2018-07-29 19:18:26 +0800

    'changed'

--- webpack-dev-config.js
+++ webpack-dev-config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 
 
+var htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: path.join(__dirname, './src/index.js'),
+    entry: {
+        index: path.join(__dirname, './src/index.js'),
+        index: path.join(__dirname, './index.html')
+    },
     output: {
         path: path.join(__dirname, './dist'),
-        filename: 'bundle.js'
+        filename: 'src/[name]-[hash].js'
     },
     module: {
         rules: [{
@@ -15,10 +19,31 @@ module.exports = {
             query: {
                 // plugins: ['transform-runtime'],
                 presets: ['es2015', 'react', 'stage-2']
-            }
-        }, {
-            test:/\.scss$/,
-            loaders:['style-loader','css-loader','sass-loader']
-        }]
-    }
+                }
+            }, {
+                test:/\.scss$/,
+                loaders:['style-loader','css-loader','sass-loader']
+            },
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                loaders: ['url-loader?limit=8192&name=images/[hash:8].[name].[ext]']
+            },
+            {
+                test: /\.png$/,
+                loaders: ["file-loader?name=images/[hash:8].[name].[ext]"]
+            },
+            {
+    　　　　　　test: /\.html$/,
+    　　　　　　loaders: ['html-withimg-loader']
+        　　}
+        ]
+    },
+    //插件数组
+    plugins:[
+    //初始化插件,传递模板参数
+    new htmlWebpackPlugin({
+            //模板为同级目录下的index.html，为何不用写路径，是因为默认上下文问webpack.config.js所在的文件夹
+                template:'index.html'
+        })
+    ]
 }
